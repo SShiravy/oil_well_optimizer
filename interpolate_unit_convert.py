abb3207197a650ac5bf70e7e7d5274f02c6bce00
Soheil <soheilprogrammer0@gmail.com>
2023-05-05 04:50:11 +0430

    everything is alright just we can't use six well data

--- interpolate_unit_convert.py
+++ interpolate_unit_convert.py
@@ -4,6 +4,7 @@ from scipy import interpolate as irp
 from read_function import all_data
 import numpy as np
 
+
 # TODO: change all_data to input af interpolation class instructor
 class Interpolation:
     def __init__(self):
@@ -16,54 +17,64 @@ class Interpolation:
     def convert_points(self, list_2bconverted, f1_add, f2_mult, f3_add):
         list = []
         for i in range(np.size(list_2bconverted)):
-            res = (list_2bconverted[i] + f1_add)*f2_mult + f3_add #Unit for converting Liquid rate
+            res = (list_2bconverted[i] + f1_add) * f2_mult + f3_add  # Unit for converting Liquid rate
             res_2 = round(res, 2)
             list.append(res_2)
         list2arr = np.array(list)
         return list2arr
-    
-    def get_points(self): 
+
+    def get_points(self):
         l = []
         for var in list(self.free_vars.keys())[::-1]:
             points_dict = []
             df_to_arr = []
             df_to_arr_converted = []
-            if var == 'rate values':
-                points_dict = self.free_vars.get('rate values') #Get points from the dictionary they're stored in. Have to get the last element first in this list for the interpolation to be correct
-                df_to_arr = np.array(points_dict) #Converts df to numpy array
-                df_to_arr_converted = self.convert_points(df_to_arr, 0, 0.158987294928, 0) #Multiplication for rate values unit conversion
-                new_arr = df_to_arr_converted.flatten() 
-            elif var == 'gl rate':
-                points_dict = self.free_vars.get('gl rate') #Get points from the dictionary they're stored in. Have to get the last element first in this list for the interpolation to be correct
-                df_to_arr = np.array(points_dict) #Converts df to numpy array
-                df_to_arr_converted = self.convert_points(df_to_arr, 0, 28173.97429124846, 0) #Multiplication for GL rate unit conversion
-                new_arr = df_to_arr_converted.flatten() 
-            elif var == 'wc': #NO conversion for WC
-                points_dict = self.free_vars.get('wc') #Get points from the dictionary they're stored in. Have to get the last element first in this list for the interpolation to be correct
-                df_to_arr = np.array(points_dict) #Converts df to numpy array
-                new_arr = df_to_arr.flatten()
-            elif var == 'gor':
-                points_dict = self.free_vars.get('gor') #Get points from the dictionary they're stored in. Have to get the last element first in this list for the interpolation to be correct
-                df_to_arr = np.array(points_dict) #Converts df to numpy array
-                df_to_arr_converted = self.convert_points(df_to_arr, 0, 0.17810760667903525, 0) #Multiplication for GOR unit conversion
+            if var == 'Rate values':
+                points_dict = self.free_vars.get(
+                    'Rate values')  # Get points from the dictionary they're stored in. Have to get the last element first in this list for the interpolation to be correct
+                df_to_arr = np.array(points_dict)  # Converts df to numpy array
+                df_to_arr_converted = self.convert_points(df_to_arr, 0, 0.158987294928,
+                                                          0)  # Multiplication for rate values unit conversion
                 new_arr = df_to_arr_converted.flatten()
-            elif var == 'pressure':
-                points_dict = self.free_vars.get('pressure') #Get points from the dictionary they're stored in. Have to get the last element first in this list for the interpolation to be correct
-                df_to_arr = np.array(points_dict) #Converts df to numpy array
-                df_to_arr_converted = self.convert_points(df_to_arr, 0, 0.0689475729, 1) #Multiplication for top node pressure unit conversion (and addition of 1)
+            elif var == 'GL rate':
+                points_dict = self.free_vars.get(
+                    'GL rate')  # Get points from the dictionary they're stored in. Have to get the last element first in this list for the interpolation to be correct
+                df_to_arr = np.array(points_dict)  # Converts df to numpy array
+                df_to_arr_converted = self.convert_points(df_to_arr, 0, 28173.97429124846,
+                                                          0)  # Multiplication for GL rate unit conversion
+                new_arr = df_to_arr_converted.flatten()
+            elif var == 'WC':  # NO conversion for WC
+                points_dict = self.free_vars.get(
+                    'WC')  # Get points from the dictionary they're stored in. Have to get the last element first in this list for the interpolation to be correct
+                df_to_arr = np.array(points_dict)  # Converts df to numpy array
+                new_arr = df_to_arr.flatten()
+            elif var == 'GOR':
+                points_dict = self.free_vars.get(
+                    'GOR')  # Get points from the dictionary they're stored in. Have to get the last element first in this list for the interpolation to be correct
+                df_to_arr = np.array(points_dict)  # Converts df to numpy array
+                df_to_arr_converted = self.convert_points(df_to_arr, 0, 0.17810760667903525,
+                                                          0)  # Multiplication for GOR unit conversion
+                new_arr = df_to_arr_converted.flatten()
+            elif var == 'Pressure':
+                points_dict = self.free_vars.get(
+                    'Pressure')  # Get points from the dictionary they're stored in. Have to get the last element first in this list for the interpolation to be correct
+                df_to_arr = np.array(points_dict)  # Converts df to numpy array
+                df_to_arr_converted = self.convert_points(df_to_arr, 0, 0.0689475729,
+                                                          1)  # Multiplication for top node pressure unit conversion (and addition of 1)
                 new_arr = df_to_arr_converted.flatten()
             l.append(new_arr)
         return l
-    
+
     def convert_to_tup(self):
         list_input = self.get_points()
         self.convert_tuple = tuple(list_input)
         return self.convert_tuple
-        
-    def get_data(self): #Get data to the points defined: Columns in TPS Res. This data should be on the regular grid in n dimensions (by def for interpolator)
+
+    def get_data(
+            self):  # Get data to the points defined: Columns in TPS Res. This data should be on the regular grid in n dimensions (by def for interpolator)
         self.pnts = self.get_points()
-        self.tpd_res_array = self.tpd_res.to_numpy()*0.0689475729 + 1
-        #The conversion under only works for col=0 or col=1: If not, the conversion is not done
+        self.tpd_res_array = self.tpd_res.to_numpy() * 0.0689475729 + 1
+        # The conversion under only works for col=0 or col=1: If not, the conversion is not done
         # If statement with many cases for new shapes (depending on number of free variables). This code accounts for: 0 <= free variables <= 6 and returns error if above
         if len(self.pnts) == 0:
             return "The number of free variables needs to be at least 1"
@@ -76,19 +87,23 @@ class Interpolation:
         elif len(self.pnts) == 3:
             self.new_dim = np.reshape(self.tpd_res_array, newshape=(len(self.pnts[0]), len(self.pnts[1]), len(self.pnts[2])))
             return self.new_dim
-        elif len(self.pnts) == 4: 
-            self.new_dim = np.reshape(self.tpd_res_array, newshape=(len(self.pnts[0]), len(self.pnts[1]), len(self.pnts[2]), len(self.pnts[3])))
+        elif len(self.pnts) == 4:
+            self.new_dim = np.reshape(self.tpd_res_array, newshape=(
+            len(self.pnts[0]), len(self.pnts[1]), len(self.pnts[2]), len(self.pnts[3])))
             return self.new_dim
         elif len(self.pnts) == 5:
-            self.new_dim = np.reshape(self.tpd_res_array, newshape=(len(self.pnts[0]), len(self.pnts[1]), len(self.pnts[2]), len(self.pnts[3]), len(self.pnts[4])))
+            self.new_dim = np.reshape(self.tpd_res_array, newshape=(
+            len(self.pnts[0]), len(self.pnts[1]), len(self.pnts[2]), len(self.pnts[3]), len(self.pnts[4])))
             return self.new_dim
         elif len(self.pnts) == 6:
-            self.new_dim = np.reshape(self.tpd_res_array, newshape=(len(self.pnts[0]), len(self.pnts[1]), len(self.pnts[2]), len(self.pnts[3]), len(self.pnts[4]), len(self.pnts[5])))
+            self.new_dim = np.reshape(self.tpd_res_array, newshape=(
+            len(self.pnts[0]), len(self.pnts[1]), len(self.pnts[2]), len(self.pnts[3]), len(self.pnts[4]),
+            len(self.pnts[5])))
             return self.new_dim
         else:
             return "The number of free variables is not between 1 and 6. The code needs adjustment in the file 'interpolation' and function 'get_data'."
 
-    #Intepolate using linear interpolation 
+    # Intepolate using linear interpolation
     def do_interpolation(self):
         self.pnts = self.convert_to_tup()
         self.data = self.get_data()
--- read_function.py
+++ read_function.py
@@ -1,3 +1,22 @@
+# import json
+# import pandas as pd
+#
+# def read_json_data(file_name):
+#     '''
+#     This funciton is to read data in the .json file using json and pandas modules
+#     :param file_name: directory + name of the data file . json
+#     :return: free variables and tpd results
+#     '''
+#     with open(file_name,'r') as data:
+#         data_dict = json.load(data)
+#         print(len(data_dict['tpd results']))
+#     free_vars = data_dict["free variables"]
+#     tpd_res = pd.Series(data_dict["tpd results"])
+#     return free_vars,tpd_res
+#
+# # print(read_json_data('data_1.json'))
+# all_data = read_json_data('wells data/Well1.json')
+
 import json
 import pandas as pd
 
