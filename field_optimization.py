17326023bc874f0c06943672f7ed6b3840955b7f
Soheil <soheilprogrammer0@gmail.com>
2023-08-08 23:39:10 +0330

    all done

--- field_optimization.py
+++ field_optimization.py
@@ -30,14 +30,16 @@ def plot_qo(fixed_free_vars, interpolate_obj, i,qgl_max):
     """
     using for loop for creating plots
     """
-    qliq_list = []
+    QO_list = []
     QGL_LIST = np.array(range(50, qgl_max,10))
     for QGL in QGL_LIST:
         free_variables = np.insert(fixed_free_vars, -2, QGL)
         free_variables = np.delete(free_variables, -2, 0)
-        qliq = list(well_production(interpolate_obj, free_variables.copy(),Q_MAX[i], i))[0]
-        qliq_list.append(qliq)
-    plt.plot(QGL_LIST, qliq_list, color='blue')
+        Qliq = list(well_production(interpolate_obj, free_variables.copy(),Q_MAX[i], i))[0]
+        WC = fixed_free_vars[2]
+        qo = Qliq * (1 - WC / 100)
+        QO_list.append(qo)
+    plt.plot(QGL_LIST, QO_list, color='blue')
     plt.xlabel("Gas Injection Rate, QGL")
     plt.ylabel("Oil Production Rate, Qliq")
     plt.savefig(f'images/qo_Well{i + 1}.png')
